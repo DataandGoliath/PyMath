c11be058f7b33376199b38c6c6e1b5c387cee018
NerdOfLinux <golf@runningwind.com>
2017-08-28 20:08:57 -0500

    Update scalc.py

--- scalc.py
+++ scalc.py
@@ -1,1 +1,10 @@
 #Simple calculator script
+#Get problem from user
+math=raw_input(": ")
+#Split up user input
+#math=math.split()
+def solve(problem):
+  solved=exec("print(%s)"%(problem))
+  return solved
+  
+print(solve(math))
