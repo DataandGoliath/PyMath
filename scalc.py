74abbe6e81531c603c57cd3ff1559e3161b8a864
NerdOfLinux <golf@runningwind.com>
2017-08-28 20:28:59 -0500

    Update scalc.py

--- scalc.py
+++ scalc.py
@@ -1,10 +1,16 @@
 from __future__ import division
+import re
+import sys
 #Simple calculator script
 #Get problem from user
 math=raw_input(": ")
 #Split up user input
 #math=math.split()
 def solve(problem):
+  #Check for letters and prevent errors
+  if re.search("[a-zA-Z+", problem):
+    print("Sorry, letters aren't allowed.")
+    sys.exit()
   #Replace '^' with '**'
   problem=problem.replace("^","**")
   solved=eval("%s"%(problem))
